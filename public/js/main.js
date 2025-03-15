858f674c979751618f1a2ef6864b6747e51ed324
Kamil <baldygakamil@gmail.com>
2025-03-15 23:55:04 +0100

    add color change on hover

--- public/js/main.js
+++ public/js/main.js
@@ -1,19 +1,37 @@
+// Variables
 let btnForm = document.querySelector(".btn");
 let inputForm = document.querySelector(".email");
 let section = document.querySelectorAll("section");
-// var card = document.querySelector(".card");
 
 btnForm.addEventListener("click", (e) => {
   e.preventDefault();
 });
 
+// Dark card on hover
 for (let i = 0; i < section.length; i++) {
   const card = section[i].children;
   for (let i = 0; i < card.length; i++) {
-    card[i].addEventListener("mouseover", () => {
+    for (let i = 0; i < card.length; i++) {
+      const element = card[i].children;
+      element[1].addEventListener("mouseover", () => {
+        element[1].classList.remove("bg-light");
+        element[1].style.backgroundColor = "grey";
+      });
+      card[1].addEventListener("mouseout", () => {
+        element[1].style.backgroundColor = "";
+      });
+    }
+  }
+}
+
+// BoxShadow
+for (let i = 0; i < section.length; i++) {
+  const card = section[i].children;
+  for (let i = 0; i < card.length; i++) {
+    card[i].addEventListener("dblclick", () => {
       card[i].style.boxShadow = "5px 5px";
     });
-    card[i].addEventListener("mouseout", () => {
+    card[i].addEventListener("click", () => {
       card[i].style.boxShadow = "0px 0px";
     });
   }
