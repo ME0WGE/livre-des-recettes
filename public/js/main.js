c73ff236e35dea64c7d463cc2d9be8258d360463
Kamil <baldygakamil@gmail.com>
2025-03-15 23:28:31 +0100

    add boxShadow for each card

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,20 @@
 let btnForm = document.querySelector(".btn");
 let inputForm = document.querySelector(".email");
+let section = document.querySelectorAll("section");
+// var card = document.querySelector(".card");
 
 btnForm.addEventListener("click", (e) => {
   e.preventDefault();
-  
 });
+
+for (let i = 0; i < section.length; i++) {
+  const card = section[i].children;
+  for (let i = 0; i < card.length; i++) {
+    card[i].addEventListener("mouseover", () => {
+      card[i].style.boxShadow = "5px 5px";
+    });
+    card[i].addEventListener("mouseout", () => {
+      card[i].style.boxShadow = "0px 0px";
+    });
+  }
+}
